b96046bca1fd9f018bb1bd52c2356427a27ac984
JoaoMathBR99 <jmatheus1991@gmail.com>
2025-03-18 19:48:28 -0300

    Update node.py

--- Listas_encadeadaas/Listas_Encadeadas/node.py
+++ Listas_encadeadaas/Listas_Encadeadas/node.py
@@ -1,1 +1,5 @@
-
+class Node: #Define uma classe chamada Node
+            #Cada nó contém um valor (item) e uma referência para o próximo nó.
+    def __init__(self, valor): #Construtor da classe
+        self.valor = valor #Atributo que armazena o valor do nó
+        self.proximo = None #Inicializa o ponteiro proximo como None
